1728c32f24f86cffc48761aede47c61c406ff709
baileyfcross <bf02cros@siena.edu>
2025-10-18 19:46:49 -0400

    updated teammanager script

--- Assets/Scripts/GenerationEngine Scripts/TeamManager.cs
+++ Assets/Scripts/GenerationEngine Scripts/TeamManager.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using static Unity.Burst.Intrinsics.X86.Avx;
 
 public class CSTeamManager : MonoBehaviour
 {
